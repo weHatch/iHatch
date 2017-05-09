87ac3a8ef595acfe50c0542c498ce8498114a572
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-05-09 00:03:19 -0600

    saving fo funzies

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,29 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('running on port 3000');
-  res.render('index', { title: 'Welcome to iHatch' });
+  const num = Math.floor((Math.random() * 4) + 1);
+  const stuff = [{
+    title: "Ideas Grow Here",
+    subtitle: "watch it become..."
+  },
+  {
+    title: "Ideas Live Here",
+    subtitle: "watch it become..."
+  },
+  {
+    title: "Now Hatching:",
+    subtitle: "It's a bird, it's a plane, it's a..."
+  },
+  {
+    title: "Creative Majesty,",
+    subtitle: "your kingdom awaits"
+  },
+  {
+    title: "Ideaize to Realize",
+    subtitle: "make it without faking it"
+  }
+]
+  res.render('index', stuff[(num)]);
 });
 
 module.exports = router;
