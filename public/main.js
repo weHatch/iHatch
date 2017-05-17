6c131cbc2a2cad7428a7efd62e978984a3c6f937
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-05-16 22:12:08 -0600

    cant link js sheet to edit route

--- public/main.js
+++ public/main.js
@@ -1,7 +1,5 @@
-var modalUp = false
-var modal = document.getElementById('myModal')
-
 $(document).ready(function(){
+
     console.log("js connected");
 
     // $('.body').css('visibility', 'visible')
@@ -10,8 +8,24 @@ $(document).ready(function(){
       $('.initialForm').toggleClass('active')
       $('.adding').toggleClass('green')
     })
+
     $('.cancel').click(function() {
       $('.initialForm').toggleClass('active')
       $('.adding').toggleClass('green')
+    })
+
+    $('.continue').click(function()  {
+      $('fadeIn1').toggleClass('active')
+      $('.continue').toggleClass('hidden')
+    })
+
+    $('.continue1').click(function() {
+      $('fadeIn2').toggleClass('active')
+      $('.continue1').toggleClass('hidden')
+    })
+
+    $('.continue2').click(function() {
+      $('fadeIn3').toggleClass('active')
+      $('.continue2').toggleClass('hidden')
     })
 })
